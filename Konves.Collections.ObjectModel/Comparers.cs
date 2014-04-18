94c9d3ae886efdf3dbb5dfb1da2d4f936d4819b1
Steve Konves <steve.konves@logos.com>
2014-04-17 22:55:25 -0700

    Add interval comparer

---
+++ Konves.Collections.ObjectModel/Comparers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Konves.Collections.ObjectModel
+{
+	public class IntervalComparer<TBound> : IComparer<IInterval<TBound>> where TBound : IComparable<TBound>
+	{
+		public int Compare(IInterval<TBound> x, IInterval<TBound> y)
+		{
+			int lowerUpper = x.LowerBound.Value.CompareTo(y.UpperBound.Value);
+			if (lowerUpper > 0 || (lowerUpper == 0 && (!x.LowerBound.IsInclusive || !y.UpperBound.IsInclusive)))
+				return 1;
+
+			int upperLower = x.UpperBound.Value.CompareTo(y.LowerBound.Value);
+			if (upperLower < 0 || (upperLower == 0 && (!x.UpperBound.IsInclusive || !y.LowerBound.IsInclusive)))
+				return -1;
+
+			return 0;
+		}
+	}
+}
